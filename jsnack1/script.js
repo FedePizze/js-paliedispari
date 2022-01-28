e1a698605b60e3daf1ab6fbe5a2c7b45ccac2529
Federico Pizzeghello <pizzeghello.work@gmail.com>
2022-01-28 15:46:25 +0100

    script jsnack 1

---
+++ jsnack1/script.js
@@ -0,0 +1,24 @@
+/*
+Chiedere all’utente di inserire una parola 
+
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+// chiedo la parola
+let text = prompt('inserisci parola palindroma');
+
+//divido la parola in lettere single e creo un array
+let split = text.split('');
+
+//faccio leggere l'array al contrario
+let reverse = split.reverse();
+
+//trasformo il contenuto dell'array reverse in una stringa
+let reverseText = reverse.join('');
+
+//creo la condizione per definire se la parole che ho scritto all'inizio è palindroma oppure no
+if(parola == reverseText){
+    alert('la parola è palindroma');
+} else {
+    alert('la parola non è palindroma');
+}
